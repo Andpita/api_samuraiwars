6e9d6d8efae73290a19b6fc792415b3b778a663f
Anderson 'Pita' Silva <dev.andpita@gmail.com>
2023-07-25 01:51:56 -0300

    api samuraiwars 2.0.2

--- dist/server.js
+++ dist/server.js
@@ -1,39 +1,7 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _socketio = require('socket.io');
-var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
-
-const whiteList = [
-  "https://samuraiwars.online",
-  "https://api.samuraiwars.online",
-  "http://localhost:4001",
-
-];
-
-const serv = new (0, _socketio.Server)('', {
-  cors: {
-    origin: (origin, callback) => {
-      if (whiteList.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ["GET", "POST"],
-  },
-});
+"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 
 const port = process.env.APP_PORT;
-const server = _app2.default.listen(port, () => {
+_app2.default.listen(port, () => {
   console.log(`Acessar http://localhost:${port}`);
-});
-
-const io = serv.listen(server);
-
-io.on('connection', (socket) => {
-  socket.on("send_message", (data) => {
-    socket.to(data.porta).emit("receive_message", data);
-  });
-
-  socket.on("msg_go", (porta) => {
-    socket.join(porta);
-  });
+  console.log('Servidor executando na porta 3001');
 });
--- src/server.js
+++ src/server.js
@@ -1,39 +1,7 @@
-import { Server } from 'socket.io';
 import app from './app';
 
-const whiteList = [
-  "https://samuraiwars.online",
-  "https://api.samuraiwars.online",
-  "http://localhost:4001",
-
-];
-
-const serv = new Server('', {
-  cors: {
-    origin: (origin, callback) => {
-      if (whiteList.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ["GET", "POST"],
-  },
-});
-
 const port = process.env.APP_PORT;
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Acessar http://localhost:${port}`);
-});
-
-const io = serv.listen(server);
-
-io.on('connection', (socket) => {
-  socket.on("send_message", (data) => {
-    socket.to(data.porta).emit("receive_message", data);
-  });
-
-  socket.on("msg_go", (porta) => {
-    socket.join(porta);
-  });
+  console.log('Servidor executando na porta 4001');
 });
